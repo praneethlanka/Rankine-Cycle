a95037a019bc80287cccc0d9ef0564139b95d1fd
Praneeth Lanka <35808849+praneethlanka@users.noreply.github.com>
2018-06-27 00:53:02 +0530

    Update reheat.m

--- reheat.m
+++ reheat.m
@@ -7,26 +7,21 @@ fprintf('Mulltiple inputs are to be given in the form of [a b c...]\n')
 rhp(:,1)=input('Enter pressure to which each cycle drops');        
 rht(:,1)=input('ENter the tmeperature before entering the reheat');
 rht(:,2)=input('\nEnter the temp. after entering the reheat''s boiler at each cycle');
-
 %Next line checks for superheated temperatures at the reheat's inlet and
 %returns 1 if it is superheated and 0 if steam is either dry saturated or in wet state. 
 rhsuph=abs(rht(:,1)-XSteam('Tsat_p',rhp(:,1)))>0.2;
-
 %If the temperature entered by the user is close to Saturation temperature
 %to a tolerance of 0.2 then it rounds it off to Saturation temperature
 rht(abs((rht(:,1)-XSteam('Tsat_p',rhp))<0.2))=XSteam('Tsat_p',abs((rhp(rht(:,1)-XSteam('Tsat_p',rhp))))<0.2);
-
 % Elementfinder returns the position of the 2nd argument of the function in
 % the 1st Argument( A matrix)
 %This line returns at what inlet is the steam is in superheated state.
 %Initially the user does not know where the steam is superheated
 [a,~]=elementfinder(rhsuph,1);
-
 %The enthalpy's at the superheated states(if any) are calculated in this
 %line. Matrix indexing is done in such a way that the pressure and
 %temperature inputs are in correspondence with the superheated states.
 rhenthsup=XSteam('h_pT',rhp(rhsuph==1),rht(rhsuph==1));
-
 % Finally the enthalpies of all the states are added into suitable
 % post=itions of the rhenth matrix with the help of output from the
 % variable a
@@ -42,17 +37,3 @@ rh{1,3}='ENTROPY';
 rh{2,1}=rht;
 rh{2,2}=rhenth;
 rh{3,3}=rhent;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
